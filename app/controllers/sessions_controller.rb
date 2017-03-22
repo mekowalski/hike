2b713b51d197c84108abde1f8845c6781f9b5369
Malind Kowalski <malind.nikulski@gmail.com>
2017-03-22 15:07:35 -0600

    small change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     hiker = Hiker.find_by(email: params[:email])
-    session[:hiker_id] = hiker.id
-    redirect_to root_url :notice => "Signed in!"
+    if hiker && hiker.authenticate(params[:password])
+      session[:hiker_id] = hiker.id
+      redirect_to root_url :notice => "You're signed in!"
+    else
+      render "new"
+    end
     # auth = request.env['omniauth.auth']
     # hiker = Hiker.find_by_provider_and_uid(auth['provider'], auth['uid']) || Hiker.create_with_omniauth(auth)
     # session[:hiker_id] = hiker.id
