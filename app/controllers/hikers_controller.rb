29c980935b6f74d76cc482b17367f105bae7d826
Malind Kowalski <malind.nikulski@gmail.com>
2017-03-24 10:55:19 -0600

    ran rake db:migrate, browser working again with links, can't figure out welcome via gh

--- app/controllers/hikers_controller.rb
+++ app/controllers/hikers_controller.rb
@@ -6,7 +6,7 @@ class HikersController < ApplicationController
   def create
     @hiker = Hiker.new(params.require(:hiker).permit(:name, :email, :password, :password_confirmation))
     if @hiker.save
-      session[:hiker_id] = hiker.id
+      session[:hiker_id] = @hiker.id
       redirect_to root_url, :notice => "You're signed up!"
     else
       render "new"
