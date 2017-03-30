c1b9d9093b0072dbebacac6d01194ee5a8859aa3
Malind Kowalski <malind.nikulski@gmail.com>
2017-03-30 15:30:11 -0600

    secured gh key/secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '7a6f6e9ce52a78196902', '24c4cae5672c771f51d8aa2dc09346b176a39441', scope: 'user'
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user'
 end
 
 
