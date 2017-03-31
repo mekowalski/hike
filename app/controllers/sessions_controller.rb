7f8bce78a8f6fdc8bad75e92db877de7ba57d4bf
Malind Kowalski <malind.nikulski@gmail.com>
2017-03-31 10:35:47 -0600

    trying to work conditional of gh/normal sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,32 @@ class SessionsController < ApplicationController
 #create is signing in the user but not giving notice,
 #and gh auth is working but not stating the 'hello' part in the application
 #also when gh auth does work, it just brings up the signin form
+  # def create
+  #   if auth = request.env['omniauth.auth']
+  #     #sign in via gh
+  #     request.env['omniauth.auth']
+  #     if hiker = Hiker.find_by_provider_and_uid(auth['provider'], auth['uid']) || Hiker.create_with_omniauth(auth)
+  #       session[:hiker_id] = hiker.id
+  #       redirect_to adventures_url :notice => "You're signed in!"
+  #     end
+  #   else
+  #     #regular sign via email/pw
+  #     hiker = Hiker.find_by(email: params[:email])
+  #     if hiker && hiker.authenticate(params[:password])
+  #       session[:hiker_id] = hiker.id
+  #       redirect_to adventures_url :notice => "You're signed in!"
+  #     else
+  #       render "new"
+  #     end
+  #   end
+  #   # else
+  #   #   auth = request.env['omniauth.auth']
+  #   #   hiker = Hiker.find_by_provider_and_uid(auth['provider'], auth['uid']) || Hiker.create_with_omniauth(auth)
+  #   #   session[:hiker_id] = hiker.id
+  #   #   redirect_to adventures_url, :notice => "Signed in!"
+  #   # end
+  # end
+
   def create
     hiker = Hiker.find_by(email: params[:email])
     if hiker && hiker.authenticate(params[:password])
