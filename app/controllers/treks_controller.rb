61d7f725d378c244ea711aa0f6b4d0a5a6f1138f
Malind Kowalski <malind.nikulski@gmail.com>
2017-04-05 14:49:36 -0600

    trying to work on nesting for trek/adventure

--- app/controllers/treks_controller.rb
+++ app/controllers/treks_controller.rb
@@ -8,12 +8,18 @@ class TreksController < ApplicationController
   end
 
   def create
-    @trek = Trek.new(params.require(:trek).permit(:name, :state, :elevation, :level, :adventure_id, :hiker_id))
+    @trek = Trek.new(trek_params)
     @trek.save
     redirect_to hiker_trek_path(current_hiker.id, trek.id)
   end
 
   def show
     @trek = Trek.find(params[:id])
+  end
+
+  private
+
+  def trek_params
+    params.require(:trek).permit(:name, :state, :elevation, :level, :adventure_id, :hiker_id, adventures_attributes: [:title], adventure_ids: [])
   end
 end
