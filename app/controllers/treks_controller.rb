cb43c7eb7b0d8ea7468e5d8e34cdf8add4cdb586
Malind Kowalski <malind.nikulski@gmail.com>
2017-04-13 12:54:51 -0600

    trek#new, trek_params, photo, fields_for

--- app/controllers/treks_controller.rb
+++ app/controllers/treks_controller.rb
@@ -5,11 +5,12 @@ class TreksController < ApplicationController
 
   def new
     @trek = Trek.new
+    @trek.build_adventure
   end
 
   def create
-    @trek = Trek.new(trek_params)
-    @trek.save
+    trek = current_hiker.treks.build(trek_params)
+    trek.save
     redirect_to hiker_trek_path(current_hiker.id, trek.id)
   end
 
@@ -20,6 +21,6 @@ class TreksController < ApplicationController
   private
 
   def trek_params
-    params.require(:trek).permit(:name, :state, :elevation, :level, :adventure_id, :hiker_id, adventures_attributes: [:title], adventure_ids: [])
+    params.require(:trek).permit(:name, :state, :elevation, :level, :adventure_id, :hiker_id, adventure_attributes: [:title])
   end
 end
