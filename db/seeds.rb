9bd35013728997d6362f040206c48d3f3d4c6d4f
Malind Kowalski <malind.nikulski@gmail.com>
2017-04-25 15:56:25 -0600

    small mods

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-counter = 0
-5.times do
-  Hiker.create(name: 'hiker' + "#{counter + 1}", email: 'hiker' + "#{counter + 1}" + "@email.com", password: 'hiker' + "#{counter + 1}")
-  Adventure.create(title: "#{counter + 1}")
-  Trek.create(name: 'trek' + "#{counter + 1}", state: 'trek' + "#{counter + 1}", elevation: 'trek' + "#{counter + 1}", level: 'trek' + "#{counter + 1}", adventure_id: counter + 1, hiker_id: counter + 1)
-  counter += 1
-end
+# counter = 0
+# 5.times do
+#   Hiker.create(name: 'hiker' + "#{counter + 1}", email: 'hiker' + "#{counter + 1}" + "@email.com", password: 'hiker' + "#{counter + 1}")
+#   Adventure.create(title: "#{counter + 1}")
+#   Trek.create(name: 'trek' + "#{counter + 1}", state: 'trek' + "#{counter + 1}", elevation: 'trek' + "#{counter + 1}", level: 'trek' + "#{counter + 1}", adventure_id: counter + 1, hiker_id: counter + 1)
+#   counter += 1
+# end
+# recreate seed to populate db
