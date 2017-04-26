0fd83d3b8add2bdc47f09921d35c3b99a99d33d1
Malind Kowalski <malind.nikulski@gmail.com>
2017-04-26 13:41:59 -0600

    field w/ errors

--- app/controllers/hikers_controller.rb
+++ app/controllers/hikers_controller.rb
@@ -7,6 +7,8 @@ class HikersController < ApplicationController
     @hiker = Hiker.new(params.require(:hiker).permit(:name, :email, :password, :password_confirmation))
     if @hiker.save
       session[:hiker_id] = @hiker.id
+      raise params.inspect
+      
       redirect_to hiker_adventures_path(current_hiker.id), :notice => "You're signed up!"
     else
       render "new"
