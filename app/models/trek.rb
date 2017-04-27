d845d418728f751edeb4872d7d984d381e7ccef2
Malind Kowalski <malind.nikulski@gmail.com>
2017-04-27 17:00:43 -0600

    PASSED ASSESSMENT. work & submit w/ 4 suggestions

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -3,7 +3,9 @@ class Trek < ApplicationRecord
   belongs_to :adventure
 
   def adventure_attributes=(adventure_attributes)
-    adventure = Adventure.find_or_create_by(adventure_attributes)
-    adventure.treks << self
+    if adventure_id != nil
+      adventure = Adventure.find_or_create_by(adventure_attributes)
+      adventure.treks << self
+    end
   end
 end
