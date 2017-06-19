c41324c6692fb2a0c2b3fab74db420185a5f4032
Malind Kowalski <malind.nikulski@gmail.com>
2017-06-19 14:15:06 -0600

    forgot to commit assessment mods

--- app/models/trek.rb
+++ app/models/trek.rb
@@ -3,9 +3,7 @@ class Trek < ApplicationRecord
   belongs_to :adventure
 
   def adventure_attributes=(adventure_attributes)
-    if adventure_id != nil
-      adventure = Adventure.find_or_create_by(adventure_attributes)
-      adventure.treks << self
-    end
+    adventure = Adventure.find_or_create_by(adventure_attributes)
+    adventure.treks << self
   end
 end
