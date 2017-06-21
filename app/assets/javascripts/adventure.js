7922e196fda571813f27ce005dc005999e0c9424
Malind Kowalski <malind.nikulski@gmail.com>
2017-06-21 10:43:59 -0600

    build constructor func, beginning proto func

--- app/assets/javascripts/adventure.js
+++ app/assets/javascripts/adventure.js
@@ -8,7 +8,26 @@ const bindClickHandlers = () => {
     e.preventDefault()
     // this is working, returning objs in console!!
     $.get('hikers/:id/adventures.json', adventures => {
-      console.log(adventures);
+      $('#main').html('')
+      adventures.forEach(adventure => {
+        // console.log(adventure)
+        let newAdventure = new Adventure(adventure)
+        console.log(newAdventure)
+      })
     })
   })
+}
+
+// constructor function
+function Adventure(adventure) {
+  this.id = adventure.id
+  this.title = adventure.title
+}
+
+// prototype function
+Adventure.prototype.formatIndex = () => {
+  let adventureHtml = `
+    <h1>${this.title}</h1>
+  `
+  return adventureHtml
 }
