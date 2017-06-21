0c3c887bff74043996fa698f7c2957689826d6af
Malind Kowalski <malind.nikulski@gmail.com>
2017-06-21 15:11:12 -0600

    pushState url finally working, pause building proto func, correct let id

--- app/assets/javascripts/adventure.js
+++ app/assets/javascripts/adventure.js
@@ -5,22 +5,25 @@ $(() => {
 
 //pushState allows append of url
 const bindClickHandlers = () => {
-  $('.nav-links').on ('click', (e) => {
+  $('.nav-links').on ('click', function(e){
     e.preventDefault();
-    var url = e.target.href;
-    history.pushState(null, null, url)
-    $.get(url, adventures => {
-      $('#main').html('')
-      $(adventures).each(adventure => {
+    let id = ($(this).data('id'))
+    // console.log($(this).data('id'));
+    history.pushState(null, null, `/hikers/${id}/adventures`) //not working
+    $.get(`/hikers/${id}/adventures.json`, adventures => {
+      $('.main').html('<h1>All Adventures</h1>')
+      console.log(adventures);
+      adventures.forEach(adventure => {
         let newAdventure = new Adventure(adventure)
         let adventureHtml = newAdventure.formatIndex()
-        $('#main').append(adventureHtml)
+        $('.main').append(adventureHtml)
       })
     })
   })
-  $(document).on('click', ".show-link", function(e) {
+  $(document).on('click', ".show-link", function(e) { //include formatShow after get
     e.preventDefault()
-    console.log(e.currentTarget);
+    let id = e.currentTarget.getAttribute('adventure') //not working
+
   })
 }
 
@@ -33,7 +36,14 @@ function Adventure(adventure) {
 // prototype function
 Adventure.prototype.formatIndex = function() {
   let adventureHtml = `
-    <a href="/adventuress/${this.id}" class="show-link"><h1>${this.title}</h1>
+    <a href="/adventures/${this.id}"  data-id="${this.id}" class="show-link"><p>${this.title}</p>
+  `
+  return adventureHtml
+}
+
+Adventure.prototype.formatShow = function() {
+  let adventureHtml = `
+    <h3>${this.title}</h3
   `
   return adventureHtml
 }
