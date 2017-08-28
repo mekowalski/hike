7d952fc5c0b5f469e7d9a0b031b0cbc0f8a362d2
Malind Kowalski <malind.nikulski@gmail.com>
2017-08-28 10:10:11 -0600

    remove adventure.js comments, delete trek.js, modify sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,11 @@ class SessionsController < ApplicationController
     hiker = Hiker.find_by(email: params[:email])
     if hiker && hiker.authenticate(params[:password])
       session[:hiker_id] = hiker.id
-      redirect_to hiker_path(current_hiker.id), :notice => "You're signed in!"
-    # elsif auth = request.env['omniauth.auth']
-    #   hiker = Hiker.find_by_provider_and_uid(auth['provider'], auth['uid']) || Hiker.create_with_omniauth(auth)
-    #   session[:hiker_id] = hiker.id
-    #   redirect_to hiker_path(current_hiker.id), :notice => "Signed in!"
-    # no use for github oauth right now
+      redirect_to adventures_path, flash[:notice] => "You're signed up!"
     else
       hiker.nil? ? (flash[:error] = 'User not found') : (flash[:error] = 'Invalid password')
       render "new"
     end
-    # session[:hiker_id] = nil
-    # was relying on hiker_id, session found but not created yet
   end
 
   def destroy
